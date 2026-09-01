81d92771a33f07a8bcfb6a8a9ddc23042e13d2e8
UserB1ank <105202277+UserB1ank@users.noreply.github.com>
2026-09-01 17:05:33 +0800

    features: 1. finish toml parse funciton

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 mod rss_parser;
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Args {
+    // Directory containing RSS resource files
+    #[arg(short,long)]
+    #[arg()]
+    resource_dir: Option<PathBuf>,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args=Args::parse();
+    let resource_dir=args.resource_dir;
+
 }
--- src/rss_parser/toml.rs
+++ src/rss_parser/toml.rs
@@ -1,34 +1,52 @@
-
+use std::fs;
+use std::path::PathBuf;
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Group{
-    name:String,
-    fees:Vec<Feed>
+pub struct TomlRss {
+    group:Vec<Group>
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Feed{
+pub struct Group{
+    name:String,
+    feed:Vec<Feed>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Feed{
     name:String,
     url:String,
-    enable:bool,
+    enabled:bool,
+}
+
+
+pub fn parse_toml(rss_path: PathBuf) -> Result<TomlRss, Box<dyn std::error::Error>>{
+    let content=fs::read_to_string(rss_path)?;
+    let toml_content: TomlRss =toml::from_str(&content)?;
+    Ok(toml_content)
 }
 
 #[cfg(test)]
 mod tests{
     use std::env;
     use std::path::PathBuf;
+    use crate::rss_parser::toml::parse_toml;
 
     #[test]
     fn test_parse_toml(){
         let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
         let project_root = PathBuf::from(manifest_dir);
-
+        let resource_dir=project_root.join("resources").join("feeds.example.toml");
+        let group=parse_toml(resource_dir).unwrap();
+        dbg!(group);
     }
     #[test]
     fn test_get_root_path(){
         let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
         let project_root = PathBuf::from(manifest_dir);
-        assert_eq!(project_root.display().to_string(),"F:\\coding\\EverydayRSS");
+        dbg!(project_root);
+        // assert_eq!(project_root.display().to_string(),"EverydayRSS");
+
     }
 }
